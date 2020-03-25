bf302b4bf2d754144b580c4e4e84d5c99f2fbf39
Vladislav Skabeev <vladskab@mail.ru>
2020-03-25 23:55:14 +0400

    New Faces

--- js/script.js
+++ js/script.js
@@ -10,15 +10,28 @@ function startGame(){
 	let killCount = 0; 
 
 	let birdsSRC = [
-		['./img/n1.png','./img/n2.png'],
-		['./img/s1.png','./img/s2.png'],
-		['./img/a1.png','./img/a2.png'],
-		['./img/t1.png','./img/t2.png'],
-		['./img/d1.png','./img/d2.png'],
+		['./img/nikita1.png','./img/nikita2.png'],
+		['./img/sasha1.png','./img/sasha2.png'],
+		['./img/anton1.png','./img/anton2.png'],
+		['./img/taras1.png','./img/taras2.png'],
+		['./img/durak1.png','./img/durak2.png'],
 		['./img/maks.png','./img/maks2.png'],
 		['./img/mikel1.png','./img/mikel2.png'],
 		['./img/seva1.png','./img/seva2.png'],
 		['./img/andrey1.png','./img/andrey2.png'],
+		['./img/ded1.png','./img/ded2.png'],
+		['./img/vika1.png','./img/vika2.png'],
+		['./img/nigina1.png','./img/nigina2.png'],
+		['./img/marina.png','./img/marina2.png'], 
+		['./img/dimych1.png','./img/dimych2.png'], 
+		['./img/yra1.png','./img/yra2.png'], 
+		['./img/emanov.png','./img/emanov2.png'],
+		['./img/kirill1.png','./img/kirill2.png'],
+		['./img/sanya.png','./img/sanya2.png'], 
+		['./img/sobol.png','./img/sobol2.png'],
+		['./img/egor.png','./img/egor2.png'],
+		['./img/slava.png','./img/slava2.png'], 
+		['./img/btm.png','./img/btm2.png'], 
 	]
 
 	let birdsIMG = []
@@ -32,7 +45,6 @@ function startGame(){
 		}
 		birdsIMG.push(subArr);
 	}
-	console.log(birdsIMG)
 
 	document.body.appendChild(cnv);
 
@@ -182,7 +194,6 @@ function startGame(){
 	function init () {
 		ctx.drawImage(backgroundGame,0,0,w,h)
 		for(i in config.group){
-			console.log(config.group[i]);
 			ctx.drawImage(config.group[i][4][drawBird(config.group[i][2])],config.group[i][0],config.group[i][1],config.bird.width,config.bird.height)
 		}
 		for(i in config.blood){
